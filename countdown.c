87c85fd7e35d9375f27299e9279c1fedf12f09c5
wakoli001 <wakoliian2007@gmail.com>
2025-10-22 18:24:04 +0300

    Add files via upload
    increment of numbers

---
+++ countdown.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ int main(){
+
+     int i = 1;
+
+     for(i = 10;i > 0; i--){
+     printf("%d\n",i);
+     }
+     printf("keep up\n\n");
+     printf("goodnight!\n\n");
+
+ return 0;
+ }
