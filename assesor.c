a825ee7c9f54530842c840fd22dcd1f6950f1adc
wakoli001 <wakoliian2007@gmail.com>
2025-10-22 18:22:17 +0300

    Add files via upload
    if if else loop

---
+++ assesor.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+   int i = 12;
+
+   if(i < 10){
+    printf("Good morning");
+   }
+   else if(i > 20){
+    printf("Good evening");
+   }
+   else{
+    printf("Good night");
+   }
+
+ return 0;
+ }
