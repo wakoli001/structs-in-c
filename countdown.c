9f7f05dd37e180046078710a188e1805e0b20c3e
Ian Wakoli <wakoliian2007@gmail.com>
2026-02-06 10:29:09 +0300

    Update countdown.c

--- countdown.c
+++ countdown.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <time h>
+
  int main(){
 
      int i = 1;
 
      for(i = 10;i > 0; i--){
+      Sleep(1000):
      printf("%d\n",i);
      }
      printf("keep up\n\n");
